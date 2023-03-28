b2781aa0835d6db5eedec161377c6898d6652df7
PhiloSolares <38137679+PhiloSolares@users.noreply.github.com>
2023-03-28 18:15:39 +0100

    Update seo_analysis_tool.py

--- seo_analysis_tool.py
+++ seo_analysis_tool.py
@@ -124,33 +124,6 @@ def visualize_clusters(words, model):
 
 
 
-def create_cluster_table_og(words, model, num_clusters=5):
-    matrix = np.zeros((len(words), model.vector_size))
-
-    for i, word in enumerate(words):
-        matrix[i, :] = model.wv[word]
-
-    # Clustering
-    kmeans = KMeans(n_clusters=num_clusters, random_state=42, n_init=10)
-    clusters = kmeans.fit_predict(matrix)
-
-    # Create a dictionary to store words per cluster
-    cluster_dict = {}
-    for i, word in enumerate(words):
-        cluster_id = clusters[i]
-        if cluster_id not in cluster_dict:
-            cluster_dict[cluster_id] = []
-        cluster_dict[cluster_id].append(word)
-
-    # Create a DataFrame from the dictionary
-    max_words = max(len(cluster_words) for cluster_words in cluster_dict.values())
-    data = {f"Cluster {i}": cluster_dict.get(i, []) + [None] * (max_words - len(cluster_dict.get(i, [])))
-            for i in range(num_clusters)}
-
-    df = pd.DataFrame(data)
-    return df
-
-
 def create_cluster_table(words, model, clusters):
     matrix = np.zeros((len(words), model.vector_size))
 
@@ -173,6 +146,7 @@ def create_cluster_table(words, model, clusters):
 
     df = pd.DataFrame(data)
     return df
+
 
 
 
@@ -222,42 +196,6 @@ def visualize_clusters_og(words, model):
     plt.title('Word Clusters based on Thematic Relatedness')
     plt.show()
 
-def visualize_clusters_plot_og(words, model):
-    matrix = np.zeros((len(words), model.vector_size))
-
-    for i, word in enumerate(words):
-        matrix[i, :] = model.wv[word]
-
-    n_clusters = 4
-    kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init=10)
-    clusters = kmeans.fit_predict(matrix)
-
-    tsne = TSNE(n_components=2, random_state=42)
-    coords = tsne.fit_transform(matrix)
-
-    x, y = coords[:, 0], coords[:, 1]
-
-    colors = cm.rainbow(np.linspace(0, 1, n_clusters))
-
-    fig, axs = plt.subplots(2, 2, figsize=(8, 8), gridspec_kw={'width_ratios': [sum(clusters == 0) + sum(clusters == 1), sum(clusters == 2) + sum(clusters == 3)], 'height_ratios': [sum(clusters == 0) + sum(clusters == 2), sum(clusters == 1) + sum(clusters == 3)]})
-    fig.subplots_adjust(wspace=0, hspace=0)
-
-    for ax in axs.ravel():
-        ax.axis('off')
-
-    for i, word in enumerate(words):
-        cluster_idx = clusters[i]
-        ax = axs[cluster_idx // 2, cluster_idx % 2]
-        ax.scatter(x[i], y[i], c=[colors[cluster_idx]], alpha=0.7)
-        ax.text(x[i], y[i], word, fontsize=10)
-
-    plt.legend(loc="best", fontsize=13)
-    plt.tight_layout()
-    temp_image_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
-    plt.savefig(temp_image_file.name, format='png')
-    plt.close()
-    return temp_image_file.name
-
 
 def visualize_clusters_plot(words, model):
     matrix = np.zeros((len(words), model.vector_size))
@@ -269,8 +207,14 @@ def visualize_clusters_plot(words, model):
     kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init=10)
     clusters = kmeans.fit_predict(matrix)
 
-    tsne = TSNE(n_components=2, random_state=42)
-    coords = tsne.fit_transform(matrix)
+    try:
+        tsne = TSNE(n_components=2, random_state=42)
+        coords = tsne.fit_transform(matrix)
+    except ValueError:
+        max_perplexity = len(words) - 1
+        tsne = TSNE(n_components=2, random_state=42, perplexity=max_perplexity)
+        coords = tsne.fit_transform(matrix)
+
 
     x, y = coords[:, 0], coords[:, 1]
 
@@ -306,8 +250,13 @@ def sanitize_url(url):
     return url
 
 
+
 # Define the inputs and outputs
-competitor_url_input = gr.inputs.Textbox(placeholder="Enter a competitor URL")
+# Define the inputs and outputs
+competitor_url_input = gr.inputs.Textbox(label="Competitor URL", placeholder="Enter a competitor URL")
+
+full_site_scrape_checkbox = gr.inputs.Checkbox(label="Tick for full site scrape (otherwise landing page only)")
+
 
 meta_tags_output = gr.outputs.Textbox(label="Meta Tags")
 heading_tags_output = gr.outputs.Textbox(label="Heading Tags")
@@ -317,7 +266,29 @@ cluster_plot_output = gr.outputs.Image(type='filepath', label="Cluster Plot")
 keyword_plot_output = gr.outputs.Image(type='filepath', label="Keyword Plot")
 seo_analysis_output = gr.outputs.Textbox(label="SEO Analysis")
 
-def analyze_website(competitor_url: str):
+def append_unique_elements(source, target):
+    for element in source:
+        if isinstance(element, Tag) and element not in target:
+            target.append(element)
+
+def get_internal_links(url: str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    internal_links = set()
+
+    for link in soup.find_all("a"):
+        href = link.get("href")
+
+        if href:
+            joined_url = urljoin(url, href)
+            parsed_url = urlparse(joined_url)
+
+            if parsed_url.netloc == urlparse(url).netloc:
+                internal_links.add(joined_url)
+
+    return internal_links
+
+def analyze_single_page(competitor_url: str):
     sanitized_url = sanitize_url(competitor_url)
     soup = get_page_content(sanitized_url)
 
@@ -406,9 +377,125 @@ def analyze_website(competitor_url: str):
 
 
 
-interface = gr.Interface(
+
+def analyze_website(competitor_url: str, full_site_scrape: bool = False):
+    
+    if not full_site_scrape:
+        topmetatags, topheadingtags, top10keywords, cluster_table, cluster_plot, keyword_plot, seo_analysis = analyze_single_page(competitor_url)
+        return topmetatags, topheadingtags, top10keywords, cluster_table, cluster_plot, keyword_plot, seo_analysis
+
+    sanitized_url = sanitize_url(competitor_url)
+    internal_links = get_internal_links(sanitized_url)
+    soup_collection = BeautifulSoup("<html><head></head><body></body></html>", "html.parser")
+    
+    for link in internal_links:
+        try:
+            soup = get_page_content(link)
+            append_unique_elements(soup.head, soup_collection.head)
+            append_unique_elements(soup.body, soup_collection.body)
+        except Exception as e:
+            print(f"Failed to analyze link: {link}. Error: {e}")
+
+    # Scrape and analyze meta tags
+    meta_tags = get_meta_tags(soup_collection)
+    topmetatags = ""
+    for name, content in meta_tags.items():
+        if "description" in name.lower():
+            topmetatags += (f"{name}: {content}\n")
+
+    # Scrape and analyze heading tags
+    heading_tags = get_heading_tags(soup_collection)
+    topheadingtags = ""
+    for tag, headings in heading_tags.items():
+        filtered_headings = [heading for heading in headings if len(heading) > 2]
+        if filtered_headings:
+            topheadingtags += (f"{tag}: {', '.join(filtered_headings)}\n")
+
+    # Scrape, analyze, and visualize keywords from page content
+    page_text = soup_collection.get_text()
+    page_text_cleaned = clean_text(page_text)
+    preprocessed_text = preprocess_text(page_text_cleaned)
+
+    keywords_counter = Counter(preprocessed_text)
+    top10keywords = ""
+
+    for keyword, count in analyze_keywords(keywords_counter, top_n=10):
+        top10keywords += (f"{keyword}: {count}\n")
+
+    # Semantic clustering and visualization
+    sentences = [preprocessed_text[i:i+10] for i in range(0, len(preprocessed_text), 10)]
+    model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, workers=4)
+
+    words = [word for word, _ in analyze_keywords(keywords_counter, top_n=50)]
+    clusters = [model.wv.doesnt_match(words)] * len(words)
+
+    cluster_plot,clusters = visualize_clusters_plot(words, model)
+    cluster_table = create_cluster_table(words, model, clusters)
+    keyword_plot = visualize_keywords(keywords_counter, top_n=10)
+
+    table_string = cluster_table.to_string(index=False)
+
+
+    heading_tags_compressed = {}
+
+    for key, values in heading_tags.items():
+        count = Counter(values)
+        sorted_values = sorted(count.keys(), key=lambda x: count[x], reverse=True)
+        filtered_values = [value for value in sorted_values if value.strip() != ""]
+        heading_tags_compressed[key] = filtered_values[:10]
+    
+
+
+
+    SEO_prompt = f"""The following information is given about a company's website:
+
+      Meta Tags:
+      {{meta_tags}}
+
+      Heading Tags:
+      {{heading_tags_compressed}}
+
+      Top 10 Keywords:
+      {{top10keywords}}
+
+      The following table represents clusters of thematically related words identified using NLP and clustering techniques. Each column represents a different cluster, and the words in each column are thematically related.
+
+      {table_string}
+
+      Please analyze the provided information and perform the following tasks:
+      1. Predict what the website is all about (the market sector).
+      2. Based on the market sector of the company, give a name to each cluster based on the theme it represents. The name needs to be the best summary of all the words in the cluster.
+      3. Perform a SWOT analysis (Strengths, Weaknesses, Opportunities, and Threats) from an SEO perspective for the company as a whole, taking into account the meta tags, heading tags, top 10 keywords, and the clusters.
+      Please provide your analysis in a clear and concise manner.
+      4. Lastly, suggest a list of 10 words and 10 phrases that the company should be using to improve their SEO
+      """.format(meta_tags=meta_tags, heading_tags_compressed=heading_tags_compressed, top10keywords=top10keywords, table_string=table_string)
+
+
+
+    def analyse_SEO(SEO_prompt):
+      response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt = SEO_prompt,
+      temperature=0.7,
+      max_tokens=1000,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+      )
+      gpt3_response = response.get('choices')[0].text
+      return gpt3_response,response
+
+
+    seo_analysis = analyse_SEO(SEO_prompt)
+
+
+    return topmetatags, topheadingtags, top10keywords, cluster_table.to_html(), cluster_plot, keyword_plot, seo_analysis[0]
+
+
+
+gr.Interface(
     fn=analyze_website,
-    inputs=competitor_url_input,
+    inputs=[competitor_url_input, full_site_scrape_checkbox],
     outputs=[
         meta_tags_output,
         heading_tags_output,
@@ -420,6 +507,6 @@ interface = gr.Interface(
     ],
     title="SEO Analysis Tool",
     description="Enter a competitor URL to perform an SEO analysis.",
-)
+    layout="vertical"
+).launch(share=True, debug=True)
 
-interface.launch(share=True)
